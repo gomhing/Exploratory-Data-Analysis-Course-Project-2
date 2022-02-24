10a329f82ba2f996b3e139ea81bbe33d443571aa
gomhing <84702818+gomhing@users.noreply.github.com>
2022-02-24 16:42:56 +0700

    Add files via upload

---
+++ plot6.R
@@ -0,0 +1,25 @@
+library("data.table")
+
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+# Extract motor vehicle emissions from the NEI data 
+condition <- grepl("vehicle", SCC[, SCC.Level.Two], ignore.case=TRUE)
+vehiclesSCC <- SCC[condition, SCC]
+vehiclesNEI <- NEI[NEI[, SCC] %in% vehiclesSCC,]
+
+# Extract motor vehicle emissions in Baltimore and LA
+vehiclesBaltimoreNEI <- vehiclesNEI[fips == "24510",]
+vehiclesBaltimoreNEI[, city := c("Baltimore City")]
+
+vehiclesLANEI <- vehiclesNEI[fips == "06037",]
+vehiclesLANEI[, city := c("Los Angeles")]
+
+# Combine both data set 
+bothNEI <- rbind(vehiclesBaltimoreNEI,vehiclesLANEI)
+
+ggplot(bothNEI, aes(x=factor(year), y=Emissions, fill=city)) +
+  geom_bar(aes(fill=year),stat="identity") +
+  facet_grid(scales="free", space="free", .~city) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))
